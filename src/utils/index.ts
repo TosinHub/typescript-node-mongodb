e3662990bb7e4a633a4975f22c52b1d67ddb5d82
TosinHub <tosinolugbenga@gmail.com>
2022-11-01 22:09:51 +0000

    update

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,9 +1,14 @@
 import 'dotenv/config'
 import 'module-alias/register'
 import ValidateEnv  from '@/utils/validateEnv'
-
+import PostController from '@/resources/post/post.controller'
 import App from './app'
 
-const app = new App([], Number(process.env.port))
+
+ValidateEnv(); 
+
+const app = new App([
+    new PostController  
+], Number(process.env.port))
 
 app.listen();
